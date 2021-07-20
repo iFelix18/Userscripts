9231f31c877df9cbd8560ff382bf8d3ee97a8880
Davide <19800006+iFelix18@users.noreply.github.com>
2021-07-20 18:27:25 +0200

    Dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 'use strict'
 
 const gulp = require('gulp')
-const tap = require('gulp-tap')
-const parser = require('userscript-parser')
 const minify = require('gulp-minify')
-const rename = require('gulp-rename')
-const merge = require('merge-stream')
 
 const paths = {
   lib: {
@@ -15,16 +11,10 @@ const paths = {
 }
 
 const compress = () => {
-  let version = ''
-
-  const release = gulp.src(paths.lib.src)
-    .pipe(tap((file) => {
-      const { meta } = parser(file.contents.toString())
-      version = meta.version[0]
-    }))
+  return gulp.src(paths.lib.src)
     .pipe(minify({
       ext: {
-        min: '.js'
+        min: '.min.js'
       },
       noSource: true,
       preserveComments: (node, comment) => {
@@ -32,30 +22,7 @@ const compress = () => {
         else return true
       }
     }))
-    .pipe(rename((path) => {
-      path.basename += `-${version}`
-      path.extname = '.min.js'
-    }))
     .pipe(gulp.dest(paths.lib.dest))
-
-  const latest = gulp.src(paths.lib.src)
-    .pipe(minify({
-      ext: {
-        min: '.js'
-      },
-      noSource: true,
-      preserveComments: (node, comment) => {
-        if (comment.value.startsWith('*')) return false
-        else return true
-      }
-    }))
-    .pipe(rename((path) => {
-      path.basename += '-latest'
-      path.extname = '.min.js'
-    }))
-    .pipe(gulp.dest(paths.lib.dest))
-
-  return merge(release, latest)
 }
 
 const watch = () => {
