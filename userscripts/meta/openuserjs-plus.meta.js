5ab256e7e48edeabe4fe6395cc2737bcb81eb6d1
Davide <19800006+iFelix18@users.noreply.github.com>
2021-08-30 15:01:56 +0200

    1.1.1

--- userscripts/meta/openuserjs-plus.meta.js
+++ userscripts/meta/openuserjs-plus.meta.js
@@ -1,12 +1,10 @@
 // ==UserScript==
-// @name            OpenUserJS+
-// @name:it         OpenUserJS+
-// @author          Davide <iFelix18@protonmail.com>
-// @namespace       https://github.com/iFelix18
-// @icon            https://www.google.com/s2/favicons?sz=64&domain=openuserjs.org
-// @description     Adds various features, and improves the OpenUserJS experience
-// @description:it  Aggiunge varie funzionalità, e migliora l'esperienza di OpenUserJS
-// @copyright       2021, Davide (https://github.com/iFelix18)
-// @license         MIT
-// @version         1.1.0
+// @name        OpenUserJS+
+// @author      Davide <iFelix18@protonmail.com>
+// @namespace   https://github.com/iFelix18
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=openuserjs.org
+// @description Adds various features, and improves the OpenUserJS experience
+// @copyright   2021, Davide (https://github.com/iFelix18)
+// @license     MIT
+// @version     1.1.1
 // ==/UserScript==
