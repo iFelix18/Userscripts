ca6e2cf928c61ae241aa3285bee9c3f1af1a4ae4
Davide <19800006+iFelix18@users.noreply.github.com>
2021-09-10 18:43:37 +0200

    Dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,36 @@
 'use strict'
 
-const gulp = require('gulp')
+const { dest, series, src, watch } = require('gulp')
+const cleanCSS = require('gulp-clean-css')
+const flatmap = require('gulp-flatmap')
+const fs = require('fs')
+const htmlmin = require('gulp-html-minifier-terser')
 const minify = require('gulp-minify')
+const replace = require('gulp-replace')
 
+// paths
 const paths = {
+  css: {
+    dest: 'template/',
+    src: 'src/template/css/*.css'
+  },
+  handlebars: {
+    dest: 'template/',
+    src: 'src/template/handlebars/*.hbs'
+  },
   lib: {
-    src: 'src/lib/**/*.js',
-    dest: 'lib/'
+    dest: 'lib/',
+    src: 'src/lib/**/*.js'
+  },
+  userscripts: {
+    dest: 'userscripts/',
+    src: 'userscripts/*.user.js'
   }
 }
 
-const compress = () => {
-  return gulp.src(paths.lib.src)
+// minify
+const minifyJS = () => {
+  return src(paths.lib.src)
     .pipe(minify({
       ext: {
         min: '.min.js'
@@ -22,14 +41,82 @@ const compress = () => {
         else return true
       }
     }))
-    .pipe(gulp.dest(paths.lib.dest))
+    .pipe(dest(paths.lib.dest))
 }
 
-const watch = () => {
-  gulp.watch(paths.lib.src, {
+const minifyCSS = () => {
+  return src(paths.css.src)
+    .pipe(cleanCSS({}))
+    .pipe(dest(paths.css.dest))
+}
+
+const minifyHandlebars = () => {
+  return src(paths.handlebars.src)
+    .pipe(htmlmin({
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      processScripts: ['text/x-handlebars-template'],
+      removeAttributeQuotes: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      sortAttributes: true,
+      sortClassName: true
+    }))
+    .pipe(dest(paths.handlebars.dest))
+}
+
+// replace
+const replaceCSS = () => {
+  return src(paths.userscripts.src)
+    .pipe(flatmap((stream, file) => {
+      return src(file.path)
+        .pipe(replace(/(?<=(css: )')(.*?)(?=')/g, fs.readFileSync('template/config.css', 'utf8')))
+        .pipe(dest('userscripts/'))
+    }))
+}
+
+const replaceHandlebars = () => {
+  return src(paths.userscripts.src)
+    .pipe(flatmap((stream, file) => {
+      const fileName = file.stem.replace('.user', '')
+
+      if (fs.existsSync(`template/handlebars/${fileName}.hbs`)) {
+        return src(file.path)
+          .pipe(replace(/(?<=(const template = )')(.*?)(?=')/g, fs.readFileSync(`template/${fileName}.hbs`, 'utf8')))
+          .pipe(dest('userscripts/'))
+      } else {
+        return stream
+      }
+    }))
+}
+
+// watch
+const watchJS = () => {
+  watch(paths.lib.src, {
     ignoreInitial: false
-  }, gulp.series(compress))
+  }, series(minifyJS))
 }
 
-exports.compress = compress
-exports.default = watch
+const watchCSS = () => {
+  watch(paths.css.src, {
+    ignoreInitial: false
+  }, series(minifyCSS, replaceCSS))
+}
+
+const watchHandlebars = () => {
+  watch(paths.handlebars.src, {
+    ignoreInitial: false
+  }, series(minifyHandlebars, replaceHandlebars))
+}
+
+// default
+const defaultTask = () => {
+  watchJS()
+  watchCSS()
+  watchHandlebars()
+}
+
+// exports
+exports.default = defaultTask
