c6b74100270f371057ad90fd0318a997fd629193
Davide <19800006+iFelix18@users.noreply.github.com>
2022-01-28 11:47:23 +0100

    1.5.2

--- src/lib/api/trakt.js
+++ src/lib/api/trakt.js
@@ -7,12 +7,10 @@
 // @description     Trakt API for my userscripts
 // @copyright       2020, Davide (https://github.com/iFelix18)
 // @license         MIT
-// @version         1.5.1
+// @version         1.5.2
 // @homepageURL     https://github.com/iFelix18/Userscripts
 // @supportURL      https://github.com/iFelix18/Userscripts/issues
 // ==/UserLibrary==
-// @connect         api.trakt.tv
-// @grant           GM.xmlHttpRequest
 // ==/UserScript==
 
 (() => {
