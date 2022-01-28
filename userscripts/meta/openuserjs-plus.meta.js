495cbc541f292323c6d4bdbf6cecf3c2133985de
Davide <19800006+iFelix18@users.noreply.github.com>
2022-01-28 01:17:28 +0100

    1.4.0

--- userscripts/meta/openuserjs-plus.meta.js
+++ userscripts/meta/openuserjs-plus.meta.js
@@ -5,5 +5,5 @@
 // @description Adds various features and improves the OpenUserJS experience
 // @copyright 2021, Davide (https://github.com/iFelix18)
 // @license MIT
-// @version 1.3.9
+// @version 1.4.0
 // ==/UserScript==
