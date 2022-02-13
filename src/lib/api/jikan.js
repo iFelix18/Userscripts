c7776305d41c043582644388c58368074cb622e6
Davide <19800006+iFelix18@users.noreply.github.com>
2022-02-13 14:41:57 +0100

    1.0.0

---
+++ src/lib/api/jikan.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @author          Davide <iFelix18@protonmail.com>
+// @namespace       https://github.com/iFelix18
+// @exclude         *
+// ==UserLibrary==
+// @name            Jikan API
+// @description     Jikan API for my userscripts
+// @copyright       2022, Davide (https://github.com/iFelix18)
+// @license         MIT
+// @version         1.0.0
+// @homepageURL     https://github.com/iFelix18/Userscripts
+// @supportURL      https://github.com/iFelix18/Userscripts/issues
+// ==/UserLibrary==
+// @connect         api.jikan.moe
+// @grant           GM.xmlHttpRequest
+// ==/UserScript==
+
+(() => {
+  /**
+   * Jikan API
+   * https://jikan.moe/
+   * @class
+   */
+  this.Jikan = class {
+    /**
+     * API configuration
+     * @param {Object}   config
+     * @param {string}  [config.url='https://api.jikan.moe']  Jikan API URL
+     * @param {boolean} [config.debug=false]                  Debug
+     */
+    constructor (config = {}) {
+      /**
+       * @private
+       */
+      this._config = {
+        url: config.url || 'https://api.jikan.moe',
+        debug: config.debug || false
+      }
+
+      /**
+       * @private
+       */
+      this._headers = {
+        'User-Agent': 'Mozilla/5.0',
+        'Content-Type': 'application/json;charset=utf-8'
+      }
+
+      /**
+       * @private
+       */
+      this._debug = (response) => {
+        if (this._config.debug || response.status !== 200) console.log(`${response.status}: ${response.finalUrl}`)
+      }
+    }
+
+    /**
+     * Returns the results of a search
+     * @param {Object}     research
+     * @param {string}    [research.search='']      Anime title to search for
+     * @param {string}    [research.type=undefined] Type of result to return
+     * @param {number}    [research.limit=25]       Limit
+     * @param {number}    [research.page=1]         Page number to return
+     * @returns {Object}
+     */
+    search (research = {}) {
+      const search = {
+        query: research.query || '',
+        type: research.type === 'series' ? 'tv' : research.type || undefined,
+        limit: research.limit || 25,
+        page: research.page || 1
+      }
+
+      return new Promise((resolve, reject) => {
+        GM.xmlHttpRequest({
+          method: 'GET',
+          url: `${this._config.url}/v4/anime?q=${encodeURIComponent(search.query)}&type=${search.type}&order_by=popularity&sfw=true&limit=${search.limit}&page=${search.page}`,
+          headers: this._headers,
+          onload: (response) => {
+            this._debug(response)
+            const data = JSON.parse(response.responseText).data
+            if (search.query !== '' && response.readyState === 4 && response.responseText !== '[]') {
+              resolve(data)
+            } else {
+              search.query === ''
+                ? reject(new Error('A search query is required.'))
+                : reject(new Error(response))
+            }
+          }
+        })
+      })
+    }
+  }
+})()
