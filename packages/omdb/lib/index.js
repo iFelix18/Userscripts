0bb0e9dfe3005d1c79c8a69c156839d9877bcf83
Davide <19800006+iFelix18@users.noreply.github.com>
2022-03-06 17:56:10 +0100

    2.0.0

--- packages/omdb/lib/index.js
+++ packages/omdb/lib/index.js
@@ -7,7 +7,7 @@
 // @description  OMDb API for my userscripts
 // @copyright    2019, Davide (https://github.com/iFelix18)
 // @license      MIT
-// @version      1.3.0
+// @version      2.0.0
 // @homepage     https://github.com/iFelix18/Userscripts/tree/master/packages/omdb#readme
 // @homepageURL  https://github.com/iFelix18/Userscripts/tree/master/packages/omdb#readme
 // @supportURL   https://github.com/iFelix18/Userscripts/issues
@@ -17,6 +17,24 @@
 // ==/UserScript==
 
 this.OMDb = (function () {
+  /**
+   * API methods
+   */
+  const methods = {
+    '/id': {
+      method: 'GET',
+      url: '/?i={id}&type={type}&y={year}&plot={plot}&tomatoes={tomatoes}'
+    },
+    '/search': {
+      method: 'GET',
+      url: '/?s={search}&type={type}&y={year}&page={page}'
+    },
+    '/title': {
+      method: 'GET',
+      url: '/?t={title}&type={type}&y={year}&plot={plot}&tomatoes={tomatoes}'
+    }
+  }
+
   /**
    * OMDb API
    *
@@ -28,19 +46,19 @@ this.OMDb = (function () {
      * API configuration
      *
      * @param {object} config Configuration
-     * @param {string} config.apikey OMDb API Key
-     * @param {string} [config.url='https://www.omdbapi.com'] OMDb API URL
+     * @param {string} config.api_key OMDb API Key
+     * @param {string} [config.api_url='https://www.omdbapi.com'] OMDb API URL
      * @param {boolean} [config.debug=false] Debug
      */
     constructor (config = {}) {
-      if (!config.apikey) throw new Error('OMDb API Key is required')
+      if (!config.api_key) throw new Error('OMDb API Key is required')
 
       /**
        * @private
        */
       this._config = {
-        apikey: config.apikey,
-        url: config.url || 'https://www.omdbapi.com',
+        api_key: config.api_key,
+        api_url: config.api_url || 'https://www.omdbapi.com',
         debug: config.debug || false
       }
 
@@ -59,47 +77,61 @@ this.OMDb = (function () {
       this._debug = (response) => {
         if (this._config.debug || response.status !== 200) console.log(`${response.status}: ${response.finalUrl}`)
       }
+
+      this._this = this
+      this._methods()
     }
 
     /**
-     * Returns the results of a search by id or title
+     * Creates the various methods
      *
-     * @param {object} research Research object
-     * @param {string} research.id A valid IMDb ID (e.g. tt1285016)
-     * @param {string} research.title Movie title to search for
-     * @param {string} [research.type=''] Type of result to return (movie, series, episode)
-     * @param {number} [research.year=''] Year of release
-     * @param {string} [research.plot='short'] Return short or full plot (short, full)
-     * @returns {object} Search results
+     * @private
      */
-    get (research = {}) {
-      if (!research.id && !research.title) throw new Error('A search query, IMDb ID or title, is required.')
+    _methods () {
+      for (const method in methods) {
+        const parts = method.split('/')
+        const function_ = parts.pop()
+        parts.shift()
 
-      const query = {
-        id: research.id || '',
-        title: research.title || '',
-        type: ['movie', 'series', 'episode'].includes(research.type) ? research.type : '',
-        year: research.year || '',
-        plot: ['short', 'full'].includes(research.plot) ? research.plot : 'short'
+        let temporary = this._this
+        for (const part of parts) {
+          temporary = temporary[part] || (temporary[part] = {})
+        }
+
+        temporary[function_] = this._request.bind(this, methods[method])
       }
+    }
+
+    /**
+     * Makes a request with GM.xmlHttpRequest
+     *
+     * @private
+     * @param {object} method API method
+     * @param {object} parameters Function parameters
+     * @returns {object} Response
+     */
+    _request (method, parameters) {
+      if (!parameters) throw new Error('Parameters is required')
 
       return new Promise((resolve, reject) => {
-        const url = query.id
-          ? `${this._config.url}/?apikey=${this._config.apikey}&i=${encodeURIComponent(query.id)}&type=${query.type}&y=${query.year}&plot=${query.plot}&r=json&tomatoes=true`
-          : `${this._config.url}/?apikey=${this._config.apikey}&t=${encodeURIComponent(query.title)}&type=${query.type}&y=${query.year}&plot=${query.plot}&r=json&tomatoes=true`
+        const finalURL = this._resolve(method, parameters)
 
         GM.xmlHttpRequest({
-          method: 'GET',
-          url,
+          method: method.method,
+          url: finalURL,
           headers: this._headers,
           timeout: 15_000,
           onload: (response) => {
             this._debug(response)
             const data = JSON.parse(response.responseText)
-            if (Object.keys(data).length > 0 && response.readyState === 4 && response.responseText !== '[]' && data.Response !== 'False') {
+            if (data.Response === 'True' && response.readyState === 4 && response.status === 200) {
               resolve(data)
             } else {
-              data.Response === 'False' ? reject(new Error(data.Error)) : reject(new Error('No results'))
+              if (data.Error) {
+                reject(new Error(data.Error))
+              } else {
+                reject(new Error('No results'))
+              }
             }
           },
           onerror: () => {
@@ -113,48 +145,38 @@ this.OMDb = (function () {
     }
 
     /**
-     * Returns the results of a search
+     * Resolve url
      *
-     * @param {object} research Research object
-     * @param {string} research.search Movie title to search for
-     * @param {string} [research.type=''] Type of result to return (movie, series, episode)
-     * @param {number} [research.year=''] Year of release
-     * @param {number} [research.page=1] Page number to return (1-100)
-     * @returns {object} Search results
+     * @private
+     * @param {object} method API method
+     * @param {object} parameters Function parameters
+     * @returns {string} Final URL
      */
-    search (research = {}) {
-      if (!research.search) throw new Error('A search query is required.')
+    _resolve (method, parameters) {
+      const url = this._config.api_url
+      const path = method.url.split('?')
+      const pathParameters = []
+      const queryString = []
 
-      const query = {
-        search: research.search || '',
-        type: ['movie', 'series', 'episode'].includes(research.type) ? research.type : '',
-        year: research.year || '',
-        page: research.page >= 1 && research.page <= 100 ? research.page : 1
+      // Path Params
+      if (path[0]) {
+        pathParameters.push(path[0])
       }
 
-      return new Promise((resolve, reject) => {
-        GM.xmlHttpRequest({
-          method: 'GET',
-          url: `${this._config.url}/?apikey=${this._config.apikey}&s=${encodeURIComponent(query.search)}&type=${query.type}&y=${query.year}&r=json&page=${query.page}`,
-          headers: this._headers,
-          timeout: 15_000,
-          onload: (response) => {
-            this._debug(response)
-            const data = JSON.parse(response.responseText)
-            if (Object.keys(data).length > 0 && response.readyState === 4 && response.responseText !== '[]' && data.Response !== 'False') {
-              resolve(data.Search)
-            } else {
-              data.Response === 'False' ? reject(new Error(data.Error)) : reject(new Error('No results'))
-            }
-          },
-          onerror: () => {
-            reject(new Error('An error occurs while processing the request'))
-          },
-          ontimeout: () => {
-            reject(new Error('Request times out'))
+      // Query String
+      if (path[1]) {
+        for (const query of path[1].split('&')) {
+          const regex = new RegExp(Object.keys(parameters).map(key => `{${key}}`).join('|'), 'gi')
+
+          if (regex.test(query)) {
+            queryString.push(query.replace(regex, (matched) => parameters[matched.replace(/[{}]/g, '')]))
           }
-        })
-      })
+        }
+      }
+      queryString.push(`apikey=${this._config.api_key}`, 'r=json') // api key and return json
+
+      // return final URL
+      return `${url}${pathParameters.join('')}?${queryString.join('&')}`
     }
   }
 
