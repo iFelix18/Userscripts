9c77cb38adc870e8253d69a0c972fc0b4f1dcc13
Davide <19800006+iFelix18@users.noreply.github.com>
2022-03-06 17:06:42 +0100

    2.0.0

--- packages/rottentomatoes/lib/index.js
+++ packages/rottentomatoes/lib/index.js
@@ -7,7 +7,7 @@
 // @description  Rotten Tomatoes API for my userscripts
 // @copyright    2022, Davide (https://github.com/iFelix18)
 // @license      MIT
-// @version      1.2.0
+// @version      2.0.0
 // @homepage     https://github.com/iFelix18/Userscripts/tree/master/packages/rottentomatoes#readme
 // @homepageURL  https://github.com/iFelix18/Userscripts/tree/master/packages/rottentomatoes#readme
 // @supportURL   https://github.com/iFelix18/Userscripts/issues
@@ -17,6 +17,20 @@
 // ==/UserScript==
 
 this.RottenTomatoes = (function () {
+  /**
+   * API methods
+   */
+  const methods = {
+    '/movie/search': {
+      method: 'GET',
+      url: '/search/?q={query}&t=movie'
+    },
+    '/tv/search': {
+      method: 'GET',
+      url: '/search/?q={query}&t=tvseries'
+    }
+  }
+
   /**
    * Rotten Tomatoes API
    *
@@ -28,7 +42,8 @@ this.RottenTomatoes = (function () {
      * API configuration
      *
      * @param {object} config Configuration
-     * @param {string} [config.url='https://www.rottentomatoes.com'] Rotten Tomatoes API URL
+     * @param {string} [config.api_url='https://www.rottentomatoes.com/api/private/v2.0'] Rotten Tomatoes API URL
+     * @param {number} [config.limit=25] Limit
      * @param {boolean} [config.debug=false] Debug
      */
     constructor (config = {}) {
@@ -36,7 +51,8 @@ this.RottenTomatoes = (function () {
        * @private
        */
       this._config = {
-        url: config.url || 'https://www.rottentomatoes.com',
+        api_url: config.api_url || 'https://www.rottentomatoes.com/api/private/v2.0',
+        limit: config.limit || 25,
         debug: config.debug || false
       }
 
@@ -55,43 +71,55 @@ this.RottenTomatoes = (function () {
       this._debug = (response) => {
         if (this._config.debug || response.status !== 200) console.log(`${response.status}: ${response.finalUrl}`)
       }
+
+      this._this = this
+      this._methods()
     }
 
     /**
-     * Returns the results of a search
+     * Creates the various methods
      *
-     * @param {object} research Research object
-     * @param {string} research.query Item title to search for
-     * @param {string} [research.type=''] Type of result to return (movie or series)
-     * @param {string} [research.limit=25] Limit
-     * @returns {object} Search results
+     * @private
      */
-    search (research = {}) {
-      if (!research.query) throw new Error('A search query is required.')
+    _methods () {
+      for (const method in methods) {
+        const parts = method.split('/')
+        const function_ = parts.pop()
+        parts.shift()
 
-      const search = {
-        query: research.query,
-        type: ['movie', 'series'].includes(research.type) ? (research.type === 'series' ? 'tvseries' : research.type) : '',
-        limit: research.limit || 25
+        let temporary = this._this
+        for (const part of parts) {
+          temporary = temporary[part] || (temporary[part] = {})
+        }
+
+        temporary[function_] = this._request.bind(this, methods[method])
       }
+    }
+
+    /**
+     * Makes a request with GM.xmlHttpRequest
+     *
+     * @private
+     * @param {object} method API method
+     * @param {object} parameters Function parameters
+     * @returns {object} Response
+     */
+    _request (method, parameters) {
+      if (!parameters.query) throw new Error('A search query is required')
 
       return new Promise((resolve, reject) => {
+        const finalURL = this._resolve(method, parameters)
+
         GM.xmlHttpRequest({
-          method: 'GET',
-          url: `${this._config.url}/api/private/v2.0/search/?q=${encodeURIComponent(search.query)}&t=${search.type}&limit=${search.limit}`,
+          method: method.method,
+          url: finalURL,
           headers: this._headers,
           timeout: 15_000,
           onload: (response) => {
             this._debug(response)
             const data = JSON.parse(response.responseText)
-            if (Object.keys(data).length > 0 && (data.movieCount > 0 || data.tvCount > 0) && response.readyState === 4 && response.responseText !== '[]') {
-              if (data.movieCount > 0 && data.tvCount > 0) {
-                resolve([...data.movies, ...data.tvSeries])
-              } else if (data.movieCount > 0) {
-                resolve(data.movies)
-              } else if (data.tvCount > 0) {
-                resolve(data.tvSeries)
-              }
+            if (response.readyState === 4 && response.status === 200) {
+              resolve(data)
             } else {
               reject(new Error('No results'))
             }
@@ -104,6 +132,44 @@ this.RottenTomatoes = (function () {
           }
         })
       })
+    }
+
+    /**
+     * Resolve url
+     *
+     * @private
+     * @param {object} method API method
+     * @param {object} parameters Function parameters
+     * @returns {string} Final URL
+     */
+    _resolve (method, parameters) {
+      const url = this._config.api_url
+      const path = method.url.split('?')
+      const pathParameters = []
+      const queryString = []
+
+      // Path Params
+      if (path[0]) {
+        pathParameters.push(path[0])
+      }
+
+      // Query String
+      if (path[1]) {
+        for (const query of path[1].split('&')) {
+          if (parameters) {
+            const regex = new RegExp(Object.keys(parameters).map(key => `{${key}}`).join('|'), 'gi')
+            queryString.push(query.replace(regex, (matched) => parameters[matched.replace(/[{}]/g, '')]))
+          } else {
+            queryString.push(query)
+          }
+        }
+      }
+      if (this._config.limit) { // limit
+        queryString.push(`limit=${this._config.limit}`)
+      }
+
+      // return final URL
+      return `${url}${pathParameters.join('')}?${queryString.join('&')}`
     }
   }
 
