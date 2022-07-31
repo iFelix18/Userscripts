278b5a4a8d0feb43d033f64a8176a19683d84919
Davide <19800006+iFelix18@users.noreply.github.com>
2022-07-31 02:56:55 +0200

    3.0.3

--- userscripts/meta/ratings-on-imdb.meta.js
+++ userscripts/meta/ratings-on-imdb.meta.js
@@ -5,5 +5,5 @@
 // @description Adds ratings from Rotten Tomatoes, Metacritic and MyAnimeList to IMDb
 // @copyright 2021, Davide (https://github.com/iFelix18)
 // @license MIT
-// @version 3.0.2
+// @version 3.0.3
 // ==/UserScript==
